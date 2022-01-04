9b050523d686af628b654d5e39f8234c42ba8e87
srsoft <neopubl@gmail.com>
2022-01-04 23:18:57 +0900

    mac

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://172.30.1.15:8000/api'
+    // baseURL: 'http://172.30.1.15:8000/api'
+    baseURL: 'http://www.immanuel.live:8000/api'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
